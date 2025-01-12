449df4590b9125e6e2d5cc49da9d2805b4dc5317
Surush Azaryun <Surush32@icloud.com>
2025-01-12 08:58:15 -0700

    Assignment done.

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <div>
       <p>Surush Azaryun</p>
-      <Link href="https://github.com/your-github-username">
+      <Link href="https://github.com/Surush32/cprg306-assignments">
         My GitHub Repository
       </Link>
     </div>
