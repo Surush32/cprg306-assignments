2507419d10dd17caf27bdc1cb5392a7ad9215436
Surush Azaryun <Surush32@icloud.com>
2025-01-12 08:50:26 -0700

    repo created

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Surush Azaryun</p>
+      <Link href="https://github.com/your-github-username">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
+} 
